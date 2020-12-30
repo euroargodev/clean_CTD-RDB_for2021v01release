27d03672951dd47ff965e89a972932a86ff761cc
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2020-12-30 10:28:02 +0100

    Add outputs for near duplicate analysis

--- a4_nearmetadup.m
+++ a4_nearmetadup.m
@@ -4,8 +4,8 @@ clear variables
 load regions.mat
 %% Near metadata duplicates
 
-outp='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\A3\';
-inp='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\A2\';
+outp='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\A4\';
+inp='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\A3\';
 % create output folder if does not exist
 if ~exist(outp, 'dir')
     mkdir(outp)
@@ -21,7 +21,7 @@ for i=1:numel(boxes)
     end
     
     % start log
-    eval(['diary ' outp 'a3_' strrep(regions{i},' ','_') '.txt'])
+    eval(['diary ' outp 'a4_' strrep(regions{i},' ','_') '.txt'])
     disp('---------------------------------------------------')
     disp(regions{i})
     disp('...')
@@ -35,35 +35,45 @@ for i=1:numel(boxes)
         % find indices of near metadata duplicates (1 decimal place for lat
         % long and 1 day diff for time)
         [ind,filein]=box_meta_neardup(inpath,box);
-        
         n=size(ind,1);
-        disp([num2str(n) ' near metadata duplicates'])
-        
-        excl=[];perc_t=[];perc_s=[];
+        disp([num2str(n) ' near metadata duplicates'])        
+        % preallocating vars
+        excl=[];
+        perc_t=zeros(n,1);perc_s=zeros(n,1);conf=zeros(n,1);
+        skip=zeros(n,1);des=zeros(n,1);
         % for each pair
         for k=1:n
             %skips pair if one member has been excluded already
             if sum(ismember(ind(k,:),excl))==0
-                 % checks if the profile is deep content duplicate (at least 90% of match below 900 m) 
-                [perc_t(k,1),perc_s(k,1),conf(k,1)]=prof_compcontdeep(filein,ind(k,:),900,95);
+                 % checks if the profile is deep content duplicate (at least 95% of match below 500 m) 
+                [perc_t(k,1),perc_s(k,1),conf(k,1)]=prof_compcontdeep(filein,ind(k,:),500,95);
                 if conf(k)==1 %if it is
                     % find the worst profile
                     w1=prof_comppc(filein,ind(k,:)); % profile content
                     w2=prof_compqc(filein,ind(k,:)); % profile qclevel/source
                     % find worst profile giving preference to the content
                     if w1==0
-                        w=w2; else
+                        w=w2;
+                        des(k)=2; % quality level
+                    else
                         w=w1;
+                        des(k)=1;% profile content
                     end
                     
                     if w==0 % if profiles are identical, delete the second
                         excl=[excl ind(k,2)];
+                         des(k)=3; % second
                     else % if not, delete the worst profile
                         excl=[excl ind(k,w)];
                     end
+                else % not duplicate
+                    des(k)=NaN; 
                 end
+                skip(k)=0;
             else %skips pair if one member has been excluded already
-                conf(k)=0;
+                conf(k)=NaN;
+                des(k)=NaN;
+                skip(k)=1;
             end
         end
         
@@ -84,16 +94,18 @@ for i=1:numel(boxes)
         end
         
         % storing indices
+        SKI{i,j}=skip;
         IND{i,j}=ind;
+        DES{i,j}=des;
+        CONF{i,j}=conf;
         PERCT{i,j}=perc_t;
         PERCS{i,j}=perc_s;
-        clear perc* conf* ind
         EXCL{i,j}=excl;
-        
+        clear perc* conf* ind des excl
         disp('...')
     end
     disp('---------------------------------------------------')
     diary off
 end
 output_label={'n nmetadup','same content','different content',' n profiles excluded'};
-save a3_results.mat boxes output* regions EXCL PER* IND;
+save a4_results.mat boxes output* regions EXCL PER* IND SKI CONF DES;
