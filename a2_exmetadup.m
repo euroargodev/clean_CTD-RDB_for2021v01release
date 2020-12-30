7685400f484acc6e3c4a031345fca75791fa92a9
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2020-12-30 10:22:04 +0100

    Add more outputs for exact duplicate analysis

--- a2_exmetadup.m
+++ a2_exmetadup.m
@@ -13,7 +13,7 @@ end
 %for each region
 for i=1:numel(boxes)
     
-    % get/ create paths 
+    % get/ create paths
     inpath=[inp regions{i} '\'];
     outpath=[outp regions{i} '\'];
     if ~exist(outpath, 'dir')
@@ -28,14 +28,16 @@ for i=1:numel(boxes)
     
     % get box list for the region
     boxlist=boxes{i};
-    
     % for each box
     for j=1:numel(boxlist)
         box=boxlist(j);
         % find indices of exact metadata duplicate
         [dup,pair,ind,filein]= box_meta_dup(inpath,box);
         n=size(ind,1);
-        excl=[];perc_t=[];perc_s=[];
+        excl=[];
+        % preallocating vars
+        perc_t=zeros(n,1);perc_s=zeros(n,1);conf=zeros(n,1);
+        skip=zeros(n,1);des=zeros(n,1);
         % for each pair
         for k=1:n
             %skips pair if one member has been excluded already
@@ -44,24 +46,32 @@ for i=1:numel(boxes)
                 [perc_t(k,1),perc_s(k,1),conf(k,1)]=prof_compcont(filein,ind(k,:));
                 if conf(k)==1 % if it is
                     % find the worst profile
-                    w1=prof_comppc(filein,ind(k,:)); % profile content 
+                    w1=prof_comppc(filein,ind(k,:)); % profile content
                     w2=prof_compqc(filein,ind(k,:)); % profile qclevel/source
-                    % find worst profile giving preference to the content
+                    % find worst profile giving preference to the content                   
                     if w1==0
-                        w=w2; else
+                        w=w2;
+                        des(k)=2; % quality level
+                    else
                         w=w1;
+                        des(k)=1;% profile content
                     end
                     
                     if w==0 % if profiles are identical, delete the second
                         excl=[excl ind(k,2)];
+                         des(k)=3; % second
                     else % if not, delete the worst profile
                         excl=[excl ind(k,w)];
                     end
                 elseif conf(k)==0 % if profile is not content duplicate
                     excl=[excl ind(k,:)]; % delete both profiles
+                    des(k)=4; % both
                 end
-            else
-                conf(k)=0; %skips pair if one member has been excluded already
+                skip(k,1)=0;
+            else %skips pair if one member has been excluded already
+                conf(k)=NaN; 
+                des(k)=NaN;
+                skip(k,1)=1;
             end
         end
         
@@ -69,7 +79,7 @@ for i=1:numel(boxes)
         % there are some profiles to delete. If not just copy the contents
         % to the outpath
         disp([num2str(n) ' exact metadata duplicates'])
-        if exist('conf','var')==1
+        if isempty(conf)==0 || sum(conf)>0
             disp(['from which ' num2str(numel(find(conf==1))) ' are also content duplicates'])
             disp(['and ' num2str(numel(find(conf==0))) ' had different contents'])
             disp([num2str(numel(excl)) ' profiles will be excluded'])
@@ -83,16 +93,19 @@ for i=1:numel(boxes)
         end
         
         %storing indices
+        SKI{i,j}=skip;
         IND{i,j}=ind;
+        DES{i,j}=des;
+        CONF{i,j}=conf;
         PERCT{i,j}=perc_t;
         PERCS{i,j}=perc_s;
-        clear perc* conf* ind
         EXCL{i,j}=excl;
-        
+        clear perc* conf* ind skip excl des ind
+                
         disp('...')
     end
     disp('---------------------------------------------------')
     diary off
 end
 output_label={'n emetadup','same content','different content',' n profiles excluded'};
-save a2_results.mat boxes output* regions EXCL PERC* IND;
+save a2_results.mat boxes output* regions EXCL PERC* IND CONF DES SKI
