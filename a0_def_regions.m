21fd3d1f284ce819285aebce756827c3960e8e82
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2021-01-06 10:39:08 +0100

    Add boxmat to easily access the results for each box

--- a0_def_regions.m
+++ a0_def_regions.m
@@ -16,7 +16,15 @@ boxes{6}=[1000 7000:7005 7101:7105 7201:7207 7301:7307 7400:7406 7501:7505 7603]
 boxes{7}=[3000:100:3500 3201:100:3501 3302:100:3502 5000:5003 5100:5103 5200:5204 5300:5305 5400:5405 5500:5505];
 boxes{8}=[3600:3602 5600:5605 3700:3702 5700:5705];
 
-save regions.mat boxes regions
+% creat a matrix 
+boxesmat=nan(numel(boxes),max(cellfun(@numel,boxes)));
+regionsmat=boxesmat;
+for i=1:numel(boxes)
+    boxesmat(i,1:numel(boxes{i}))=boxes{i};
+    regionsmat(i,1:numel(boxes{i}))=repmat(i,1,numel(boxes{i}));
+end
+
+save regions.mat boxes regions boxesmat regionsmat
 
 %% plot regions
 cmp=hsv(8);
