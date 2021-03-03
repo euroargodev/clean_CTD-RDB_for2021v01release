3d6fd73f2b0029a76c97fa72d8c267aed6745ac4
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2021-03-03 10:06:36 +0100

    add scripts to clean all data sources

--- cleaninput/BSHICES_allclean.m
+++ cleaninput/BSHICES_allclean.m
@@ -1,5 +1,7 @@
+addpath '\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020'
+addpath(genpath('\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\imab\'))
 clear variables
-inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\uices_2015_2019\';
+inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\CTD_for_2019\wmo_boxes\uices_2015_2019\';
 
 list=dir([inpath 'ctd*.mat']);
 for i=1:numel(list)
@@ -8,31 +10,55 @@ for i=1:numel(list)
     boxlist(i)=str2double(str{1});
 end    
 
+boutpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\BSH\';
+outpath=[boutpath 'A0\'];
+if ~exist(outpath, 'dir')
+mkdir(outpath)
+end
+
 for i=1:numel(list)
     load([inpath list(i).name],'qclevel')
     for k=1:numel(qclevel)
         qclevel{k}='BSH';
     end
-    save([inpath list(i).name],'qclevel','-append')
+    box_copy(inpath,boxlist(i),outpath)
+    save([outpath list(i).name],'qclevel','-append')
 end
 
-matfilesdir=inpath;
+matfiledir=boutpath;
 
-outp=inpath;
-outpath=outp;
-F1_basic_corr(boxlist,inpath,outpath,[matfilesdir 'step1.mat']);
 inpath=outpath;
-outpath=[outp 'A1\'];
-F2_exmetadup(boxlist,inpath,outpath,[matfilesdir 'step2.mat']);
+outpath=[boutpath 'A1\'];
+if ~exist(outpath, 'dir')
+mkdir(outpath)
+end
+F1_basic_corr(boxlist,inpath,outpath,[matfiledir 'step1.mat']);
+
 inpath=outpath;
-outpath=[outp 'A2\'];
-F3_contdup(boxlist,inpath,outpath,[matfilesdir 'step3.mat']);
+outpath=[boutpath 'A2\'];
+if ~exist(outpath, 'dir')
+mkdir(outpath)
+end
+F2_exmetadup(boxlist,inpath,outpath,[matfiledir 'step2.mat']);
+
 inpath=outpath;
-outpath=[outp 'A3\'];
-F4_nearmetadup(boxlist,inpath,outpath,[matfilesdir 'step4.mat']);
+outpath=[boutpath 'A3\'];
+if ~exist(outpath, 'dir')
+mkdir(outpath)
+end
+F3_contdup(boxlist,inpath,outpath,[matfiledir 'step3.mat']);
+
+inpath=outpath;
+outpath=[boutpath 'A4\'];
+if ~exist(outpath, 'dir')
+mkdir(outpath)
+end
+F4_nearmetadup(boxlist,inpath,outpath,[matfiledir 'step4.mat']);
+
 inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\uices_2015_2019\';
-F5_get_orindices([matfilesdir 'step1.mat'],...
-    [matfilesdir 'step2.mat'],...
-    [matfilesdir 'step3.mat'],...
-    [matfilesdir 'step4.mat'],...
-    [matfilesdir 'indices_steps.mat'])
+
+F5_get_orindices([matfiledir 'step1.mat'],...
+    [matfiledir 'step2.mat'],...
+    [matfiledir 'step3.mat'],...
+    [matfiledir 'step4.mat'],...
+    [matfiledir 'indices_steps.mat'])
--- cleaninput/UDASH_allclean.m
+++ cleaninput/UDASH_allclean.m
@@ -1,29 +1,37 @@
+addpath '\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020'
+addpath(genpath('\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\imab\'))
+addpath '\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\matlab_toolboxes\m_map'
 clear variables
-inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\udash\';
-load([inpath 'udash_2021'],'valid_boxes')
+inpath1='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\udashprep\';
+load([inpath1 'udash_2021'],'valid_boxes')
 boxlist=valid_boxes;clear valid_boxes
 
+inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\UDASH\A0\';
+
 for i=1:numel(boxlist)
-    load([inpath 'ctd_' num2str(i) '.mat'],'qclevel')
+    filename=['ctd_' num2str(boxlist(i)) '.mat'];
+    load([inpath filename ],'qclevel')
     for k=1:numel(qclevel)
         qclevel{k}='UDA';
     end
-    save([inpath list(i).name],'qclevel','-append')
+    save([inpath filename],'qclevel','-append')
 end
 
-matfilesdir=inpath;
+matfilesdir='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\UDASH\';
+
+inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\UDASH\';
 
 outp=inpath;
-outpath=outp;
+outpath=[outp 'A1\'];
 F1_basic_corr(boxlist,inpath,outpath,[matfilesdir 'step1.mat']);
 inpath=outpath;
-outpath=[outp 'A1\'];
+outpath=[outp 'A2\'];
 F2_exmetadup(boxlist,inpath,outpath,[matfilesdir 'step2.mat']);
 inpath=outpath;
-outpath=[outp 'A2\'];
+outpath=[outp 'A3\'];
 F3_contdup(boxlist,inpath,outpath,[matfilesdir 'step3.mat']);
 inpath=outpath;
-outpath=[outp 'A3\'];
+outpath=[outp 'A4\'];
 F4_nearmetadup(boxlist,inpath,outpath,[matfilesdir 'step4.mat']);
 inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\udash\';
 F5_get_orindices([matfilesdir 'step1.mat'],...
