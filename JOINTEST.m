fbc77b94d30b0cc025cf7cd69d1412165734275b
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2021-03-03 10:08:36 +0100

    add scripts to join folders and check for duplicates

--- JOINTEST.m
+++ JOINTEST.m
@@ -1,12 +1,73 @@
-box=1600;
-ipath{1}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\check2020V01\A6\';
-ipath{2}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\udash\A4\';
-ipath{3}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\io_pan\';
-\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\CTD_for_2019\wmo_boxes\uices_2015_2019
-opath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\jointest\';
-indup=join_boxes(ipath,opath,box);
+clear variables
+%boxlist
+inpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\UDASH\A4\';
+list=dir([inpath 'ctd*.mat']);
+for i=1:numel(list)
+    str=strsplit(list(i).name,'_');
+    str=strsplit(str{2},'.');
+    boxlist(i)=str2double(str{1});
+end    
 
-outpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\CTD-RDB_2020\';
-boxlist(1)=box;
+ipath{1}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\check2020V01\A6\';
+ipath{2}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\BSH\A4\';
+ipath{3}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\IOPAN\A4\';
+ipath{4}='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\UDASH\A4\';
+
+opath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\jointest\';
+
+% get variables list
+in=ipath{4};
+filein=[in 'ctd_7700.mat'];
+var_list=whos('-file',filein);
+nvar=numel(var_list);
+strv=[];
+for i=1:nvar
+    if i==nvar
+        strv=[strv ' ' var_list(i).name];
+    else
+        strv=[strv var_list(i).name ' '];
+    end
+end
+vars.var_list=strsplit(strv,' ');
+vars.nvar=numel(var_list);
+vec=[1:3 6 8];
+for i=1:numel(vec)
+    vars.var_vect{i}=var_list(vec(i)).name;
+end
+mat=[4 5 7 9];
+for i=1:numel(mat)
+    vars.var_mat{i}=var_list(mat(i)).name;
+end
+vars.strv=strv;
+
+for i=1:numel(boxlist)
+    indup{i}=join_boxes(ipath,vars,opath,boxlist(i));
+end
+% 
+outpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\jointest\A2\';
 F2_exmetadup(boxlist,opath,outpath,'testdup1.mat',indup)
 
+list=dir([outpath '*.mat']);
+for i=1:numel(list)
+    m=matfile([outpath list(i).name]);
+    if isempty(whos(m,'ind*'))==0
+        indup{i}=m.indup;
+    end
+end
+
+opath=outpath;
+outpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\jointest\A3\';
+F3_contdup(boxlist,opath,outpath,'testdup2.mat',indup)
+
+
+list=dir([outpath '*.mat']);
+for i=1:numel(list)
+    m=matfile([outpath list(i).name]);
+    if isempty(whos(m,'ind*'))==0
+        indup{i}=m.indup;
+    end
+end
+
+opath=outpath;
+outpath='\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CTD-RDB-DMQC\2020\jointest\A4\';
+F4_nearmetadup(boxlist,opath,outpath,'testdup3.mat',indup)
--- join_boxes.m
+++ join_boxes.m
@@ -1,46 +1,52 @@
-function indup=join_boxes(ipath,opath,box)
+function indup=join_boxes(ipath,vars,opath,box)
+
+var_list=vars.var_list;
+nvar=vars.nvar;
+var_vect=vars.var_vect;
+var_mat=vars.var_mat;
+strv=vars.strv;
 
 n=numel(ipath);
-
-% get variables list
-in=ipath{1};
-filein=[in 'ctd_' num2str(box)];
-var_list=whos('-file',filein);
-nvar=numel(var_list);
-strv=[];
-for i=1:nvar
-    strv=[strv var_list(i).name ' '];
-end
-
 fileout=[opath 'ctd_' num2str(box)];
 
 % loop for variables
-for i=1:nvar
-    eval(['load ' filein ' ' var_list(i).name])
-    if var_list(i).size(1) ==1 % vector vars
-        eval(['tmp=' var_list(i).name ';' ])
-        for k = 2:n
+for i=1:nvar    
+    if isempty(find(contains(var_vect,var_list{i})))==0 % vector vars
+        % loop for paths
+        for k = 1:n
             in2=ipath{k};
-            fileup=[in2 'ctd_' num2str(box)];
-            eval(['load ' fileup ' ' var_list(i).name])
-            if k==n
-                eval([var_list(i).name '= [tmp ' var_list(i).name '];' ])
-            else
-                eval(['tmp=[tmp ' var_list(i).name '];' ])
+            fileup=[in2 'ctd_' num2str(box) '.mat'];
+            if exist(fileup,'file')==2
+                eval(['load ' fileup ' ' var_list{i}])
+                if exist('tmp','var')
+                    eval(['tmp=[tmp ' var_list{i} '];' ])
+                else
+                    eval(['tmp= ' var_list{i} ';' ])
+                end
             end
         end
+        eval([var_list{i} '= tmp;' ])
+        clear tmp
+        
     else % matrix variables.
         % the number of rows of the new data must be the same as the matrix
         % already in the file (or viceversa)
         
         for k = 1:n
-            fileup=[ipath{k} 'ctd_' num2str(box)];
-            eval(['load ' fileup ' ' var_list(i).name])
-            eval(['tmpm{k}=' var_list(i).name ';'])
-            s(k,:)=size(tmpm{k});            
+            in2=ipath{k};
+            fileup=[in2 'ctd_' num2str(box) '.mat'];           
+            if exist(fileup,'file')==2
+                eval(['load ' fileup ' ' var_list{i}])
+                if exist('tmp','var')
+                    eval(['tmpm{k}=' var_list{i} ';'])
+                else
+                    eval(['tmpm{k}= ' var_list{i} ';' ])
+                end
+                s(k,:)=size(tmpm{k});
+            end
         end
         tmpm=cell2fillnan(tmpm);
-        eval([var_list(i).name '= tmpm;' ])  
+        eval([var_list{i} '= tmpm;' ])
         clear tmpm
     end
 end
@@ -49,7 +55,7 @@ for k=1:n
     indup{k}=k*ones(1,s(k,2));
 end
 indup=cell2mat(indup);
-strv=[strv 'indup'];
+strv=[strv ' indup'];
 
 long=convertlon(long,360);
 
