82c9e7270cd9213cee13e7bc743f96ab683dfe22
Ingrid M. Angel-Benavides <ingrid.angel@bsh.de>
2023-01-31 17:02:48 +0100

    removed all functions not called by example_script and not commented yet

--- b2_bulk_dupanalysis.m
+++
@@ -1,4 +0,0 @@
-addpath '\\win.bsh.de\root$\Standard\Hamburg\Homes\Homes00\bm2286\CodeProjects\matlab_toolboxes\export_fig'
-warning('off','MATLAB:ui:javaframe:PropertyToBeRemoved')
-[qckept,qcexcluded,criteria1,criteria2,indkept,indexcluded,boxn]...
-    =plot_dupanalysis(2,[],[],[],[],1,1);
